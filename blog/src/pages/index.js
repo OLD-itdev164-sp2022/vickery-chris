3a924901368f78d4f469747cd74f390af73c2e0c
Chris Vickery <vickeryc@gmail.com>
2022-03-18 13:22:49 -0500

    added componets for list, and listItem to rpleace ul, li changed ul, and li's on home page respectivly

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -5,13 +5,14 @@ import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { List, ListItem } from "../components/List"
 
 const IndexPage = ({ data }) => (
   <Layout>
     <Seo title="Home" />
-    <ul>
+    <List width={[1, 1 / 2, 2 / 3]} p={2}>
       {data.allContentfulBlogPost.edges.map(edge => (
-        <li>
+        <ListItem p={3}>
           <Link to={edge.node.slug} key={edge.node.id}>
             {edge.node.title}
           </Link>
@@ -20,9 +21,9 @@ const IndexPage = ({ data }) => (
             alt = "test"
           </div>
           <div>{edge.node.body.childMarkdownRemark.excerpt}</div>
-        </li>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   </Layout>
 )
 
@@ -45,7 +46,7 @@ export const query = graphql`
             gatsbyImageData(
               layout: CONSTRAINED
               placeholder: BLURRED
-              width: 300
+              width: 600
             )
           }
         }
